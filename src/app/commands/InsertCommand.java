2cf9df913f3c9bc41de0dd5ea6207e672ec09b07
komitoff <todorkomitov@gmail.com>
2017-11-19 15:39:40 +0200

    Finished insert command

--- src/app/commands/InsertCommand.java
+++ src/app/commands/InsertCommand.java
@@ -1,5 +1,6 @@
 package app.commands;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InsertCommand extends Command {
@@ -11,7 +12,28 @@ public class InsertCommand extends Command {
     @Override
     public String execute(String command) {
 
-        //todo.........
+        String[] input = command.split("\\s+");
+        List<String> changedList = new ArrayList<>();
+
+        if(input.length != 3){
+            throw new UnsupportedOperationException("Error: invalid command parameters");
+        }
+
+        int index = Integer.valueOf(input[1]);
+        String element = input[2];
+
+        if (index < 0 || index >= getStrings().size()) {
+            throw new UnsupportedOperationException("Error: invalid index " + index);
+        }
+
+        for (int i = index; i < getStrings().size() ; i++) {
+            changedList.add(getStrings().get(i));
+        }
+        getStrings().removeAll(changedList);
+        getStrings().add(element);
+        for (int i = 0; i < changedList.size(); i++) {
+            getStrings().add(changedList.get(i));
+        }
 
         return super.toString();
     }
